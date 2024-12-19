1477d24a2a3561df4ecdf896bab0393b51df8bec
888-coding <pachelee@gmail.com>
2024-12-19 09:54:59 -0300

    create_database.py: Adicionando condicao para criar BANCO DADOS

--- create_database.py
+++ create_database.py
@@ -1,3 +1,16 @@
 # Criação de Banco de dados 
 # - Fazer condição para não ter duplicidade
+# - Nome do banco de dados : contabilidade.db 
+
+
+import os
+
+nome_arquivo = "contabilidade.db"
+
+if os.path.exists(nome_arquivo):
+    print("O arquivo existe")
+else:
+    print("Não existe arquivo")
+    # Criação de banco de dados
+
 
