d266c3c52bf9167969c0b999b1211329504ae52b
888-coding <pachelee@gmail.com>
2024-12-19 10:12:02 -0300

    create_database.py: Adicionado biblioteca SQLITE3 && Criação de banco de dados

--- create_database.py
+++ create_database.py
@@ -2,15 +2,16 @@
 # - Fazer condição para não ter duplicidade
 # - Nome do banco de dados : contabilidade.db 
 
-
 import os
+import sqlite3 as s
 
 nome_arquivo = "contabilidade.db"
 
 if os.path.exists(nome_arquivo):
-    print("O arquivo existe")
+    print("O arquivo já existe")
 else:
-    print("Não existe arquivo")
     # Criação de banco de dados
-
+    conexao = s.connect("contabilidade.db")
+    conexao.close()
+    print("Criação do arquivo com sucesso")
 
