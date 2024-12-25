0833a380eb2ac6972cacb580f9c0726d08eafd68
888-coding <pachelee@gmail.com>
2024-12-24 23:30:36 -0300

    Adicionando condição para criar tabela de usuarios

--- create_tables.py
+++ create_tables.py
@@ -3,3 +3,27 @@
 ## 2. CONTAS
 ## 3. MOVIMENTOS 
 
+### SQL para checar se existe tabela: SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'; 
+
+import os 
+import sqlite3
+
+nome_arquivo = "contabilidade.db"
+if os.path.exists(nome_arquivo):
+    print("O banco de dados existe")
+    conexao = sqlite3.connect('contabilidade.db')
+    cursor = conexao.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='usuarios'")
+    resultado = cursor.fetchone()
+    cursor.close()
+    
+    if resultado == None:
+        print("A tabela não existe")
+        cursor = conexao.cursor()
+        cursor.execute("CREATE TABLE usuarios (id integer, nome text, senha text)")
+        cursor.close()    
+    else:
+        print("A tabela usuarios já existe!") 
+else: 
+    print("O banco de dados não existe! ")
+
