3874495e159e222f234e1c3be35e5f6ded1293d6
888-coding <pachelee@gmail.com>
2024-12-28 20:19:32 -0300

    Montando o menu no main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py 
+
+print("====== SISTEMA KINKIN CONTÁBIL =====")
+print("====================================")
+print("MENU DO SISTEMA : ------------------")
+print("1 - ACESSAR CONTAS ")
+print("2 - ACESSAR MOVIMENTAÇÕES ")
+print("3 - ACESSAR RELATÓRIOS ")
+print("====================================")
+
+
