c22f5cf1ee887bd50306cc4d6562bc838a904d06
888-coding <pachelee@gmail.com>
2024-12-29 00:09:52 -0300

    Inserido para dentro de funcao main

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # main.py 
 
-print("====== SISTEMA KINKIN CONTÁBIL =====")
-print("====================================")
-print("MENU DO SISTEMA : ------------------")
-print("1 - ACESSAR CONTAS ")
-print("2 - ACESSAR MOVIMENTAÇÕES ")
-print("3 - ACESSAR RELATÓRIOS ")
-print("====================================")
 
-while True:
-    opcao_menu = input("> Escolha uma opção no menu: ")
-    if opcao_menu.isdigit():
-        if int(opcao_menu) < 4 and int(opcao_menu) > 0 :
-            opcao_menu = int(opcao_menu)
-            break
+def main():
+    print("====== SISTEMA KINKIN CONTÁBIL =====")
+    print("====================================")
+    print("MENU DO SISTEMA : ------------------")
+    print("1 - ACESSAR CONTAS ")
+    print("2 - ACESSAR MOVIMENTAÇÕES ")
+    print("3 - ACESSAR RELATÓRIOS ")
+    print("====================================")
+
+    while True:
+        opcao_menu = input("> Escolha uma opção no menu: ")
+        if opcao_menu.isdigit():
+            if int(opcao_menu) < 4 and int(opcao_menu) > 0 :
+                opcao_menu = int(opcao_menu)
+                break
+            else:
+                print("Escolha a opção certa: ")
         else:
             print("Escolha a opção certa: ")
-    else:
-        print("Escolha a opção certa: ")
+
+    print(f"A Opção escolhida foi : {opcao_menu}")
+main()
