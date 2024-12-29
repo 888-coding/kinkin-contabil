b39c5a03608c00733b1bb9cead93a17a22336882
888-coding <pachelee@gmail.com>
2024-12-28 23:10:44 -0300

    Adicionado condição para escolha de menu

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ print("2 - ACESSAR MOVIMENTAÇÕES ")
 print("3 - ACESSAR RELATÓRIOS ")
 print("====================================")
 
-
+while True:
+    opcao_menu = input("> Escolha uma opção no menu: ")
+    if opcao_menu.isdigit():
+        if int(opcao_menu) < 4 and int(opcao_menu) > 0 :
+            opcao_menu = int(opcao_menu)
+            break
+    else:
+        print("Não digitou certo!")
