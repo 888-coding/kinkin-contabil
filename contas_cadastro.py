7b2d82fe01a1bdf3046b9ea22a1dec835cf0a432
888-coding <pachelee@gmail.com>
2025-01-04 09:35:19 -0300

    Montando tela de cadastro de Contas.

--- contas_cadastro.py
+++ contas_cadastro.py
@@ -1,7 +1,16 @@
 # contas_cadastro.py 
 
 import sqlite3 as s 
+from conexao import bancodados_nome
 
 def cadastrar():
     print("Estou dentro da funcao cadastrar")
+    tela_do_cadastro()
 
+    
+def tela_do_cadastro():
+    print("Tela de cadastro de Contas : ")
+    base = input("Inserir a Conta BASE         : ")
+    complementar = input("Inserir a conta COMPLEMENTAR : ")
+    descricao = input("Inserir o nome da Conta      : ")
+    print(f"A conta base é {base} , e a conta complementar é {complementar} : {descricao}")
