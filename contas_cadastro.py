47986bd121a7c61ab3616bb000c307c8762c1dac
888-coding <pachelee@gmail.com>
2025-01-04 23:49:44 -0300

    Colocado condições para que não erre o numero Base. (cadastro de contas)

--- contas_cadastro.py
+++ contas_cadastro.py
@@ -10,15 +10,24 @@ def cadastrar():
     
 def tela_do_cadastro():
     print("Tela de cadastro de Contas : ")
-    base = input("Inserir a Conta BASE         : ")
+    while True: 
+        base = input("Inserir a Conta BASE         : ")
+        if base.isdigit():
+            print("O número base é digito!")
+            base = int(base)
+            if base < 1 or base > 9999 :
+                print("Favor escolher tamanho de 4 digitos.")
+            else:
+                print("Base está correto!!")
+                break
+        else:
+            print("O número base não é digito!")
+    
     complementar = input("Inserir a conta COMPLEMENTAR : ")
     descricao = input("Inserir o nome da Conta      : ")
     print(f"A conta base é {base} , e a conta complementar é {complementar} : {descricao}")
     #CONTA BASE DEVE SER NUMERO DE ATÉ 4 CASAS.
     # ... Não pode ser mais que 9999. 
 
-    if base.isdigit():
-        print("O número base é digito!")
-    else:
-        print("O número base não é digito!")
+    
     
