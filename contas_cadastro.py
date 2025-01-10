2b50f0ff057975e90249e6a88223e78f19097ad4
888-coding <pachelee@gmail.com>
2025-01-10 07:02:33 -0300

    Criando o cadastro de contas

--- contas_cadastro.py
+++ contas_cadastro.py
@@ -5,9 +5,8 @@ from conexao import bancodados_nome
 
 def cadastrar():
     print("Estou dentro da funcao cadastrar")
-    tela_do_cadastro()
-
-    
+    dados_para_cadastro = tela_do_cadastro()
+    cadastrar_conta(dados_para_cadastro)
 def tela_do_cadastro():
     print("Tela de cadastro de Contas : ")
     # Checar o número Base 
@@ -23,7 +22,17 @@ def tela_do_cadastro():
                 break
         else:
             print("O número base não é digito!")
-    
+
+    base = str(base)
     complementar = input("Inserir a conta COMPLEMENTAR : ").upper()
     descricao = input("Inserir o nome da Conta      : ").upper()
-    print(f"A conta base é {base} , e a conta complementar é {complementar} : {descricao}")
+    dados_para_cadastro = [base, complementar, descricao]
+    return dados_para_cadastro
+def cadastrar_conta(dados):
+    base = int(dados[0])
+    complementar = dados[1]
+    descricao = dados[2]
+    conexao = s.connect(bancodados_nome)
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO contas (conta_base, conta_complementar, conta_descricao) VALUES (" & base & ", ")
+    conexao.commit()
