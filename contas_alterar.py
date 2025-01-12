69540aaa10d3fe0e2aee299aff461f41f3481323
888-coding <pachelee@gmail.com>
2025-01-12 09:52:49 -0300

    Criando a tela para cadastro.

--- contas_alterar.py
+++ contas_alterar.py
@@ -1,1 +1,13 @@
 #contas_alterar.py
+import sqlite3 as s
+from conexao import bancodados_nome
+
+def tela_contas_alterar():
+    print(">  Alterar uma conta.")
+    print("---------------------")
+
+    print("Qual conta deseja alterar ?")
+    base = input("Conta base :  ")
+    complementar = input("Conta complementar :  ")
+    dados_consultar = [base, complementar]
+    print(dados_consultar)
