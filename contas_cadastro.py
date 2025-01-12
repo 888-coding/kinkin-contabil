a52bbb6169c31d3c7bde7ec9d4b9ff800fdad5f2
888-coding <pachelee@gmail.com>
2025-01-12 00:41:50 -0300

    Foi feito a verificação de contas antes de cadastrá-lo, no contas_cadastro.py

--- contas_cadastro.py
+++ contas_cadastro.py
@@ -4,9 +4,12 @@ import sqlite3 as s
 from conexao import bancodados_nome
 
 def cadastrar():
-    print("Estou dentro da funcao cadastrar")
     dados_para_cadastro = tela_do_cadastro()
-    cadastrar_conta(dados_para_cadastro)
+    deseja_cadastrar = verificar_existencia_conta(dados_para_cadastro)
+    if deseja_cadastrar == "S":
+        cadastrar_conta(dados_para_cadastro)
+    if deseja_cadastrar == "N":
+        print("Não foi cadastrado.")
 def tela_do_cadastro():
     print("Tela de cadastro de Contas : ")
     # Checar o número Base 
@@ -28,13 +31,29 @@ def tela_do_cadastro():
     descricao = input("Inserir o nome da Conta      : ").upper()
     dados_para_cadastro = [base, complementar, descricao]
     return dados_para_cadastro
+
+def verificar_existencia_conta(dados):
+    conexao = s.connect(bancodados_nome)
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM contas WHERE conta_base = ? AND conta_complementar = ?", (dados[0], dados[1]))
+    resultados = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+
+    if resultados:
+        print("Já existe cadastro !")
+        deseja_cadastrar = "N"
+    else:
+        deseja_cadastrar = input("Não existe cadastro, deseja cadastrar ? S/N :  ").upper()
+    return  deseja_cadastrar
 def cadastrar_conta(dados):
     base = dados[0]
     complementar = dados[1]
     descricao = dados[2]
     conexao = s.connect(bancodados_nome)
-    #print("INSERT INTO contas (conta_base, conta_complementar, conta_descricao) VALUES (" & base & ", '" & complementar & "', '" & descricao & "')")
     cursor = conexao.cursor()
-    # cursor.execute("INSERT INTO contas (conta_base, conta_complementar, conta_descricao) VALUES (" and base and ", '" and complementar and "', '" and descricao and "')")
     cursor.execute("INSERT INTO contas(conta_base, conta_complementar, conta_descricao) VALUES (?, ?, ?)", (base, complementar, descricao))
     conexao.commit()
+    cursor.close()
+    conexao.close()
+    print("Cadastrado com sucesso.")
