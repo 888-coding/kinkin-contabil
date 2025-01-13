260c307dbd4250e2cdd8f9742aedf386b4409e22
888-coding <pachelee@gmail.com>
2025-01-13 00:16:01 -0300

    Corrigido contas_alterar.py para preparar para alterar a conta.

--- contas_alterar.py
+++ contas_alterar.py
@@ -2,27 +2,36 @@
 import sqlite3 as s
 from conexao import bancodados_nome
 def alterar():
-    tela_contas_alterar()
+    dados_consultar = tela_contas_alterar()
+    conta_id = consultar_conta(dados_consultar)
+    print("Encontramos a conta !  ")
+    deseja_alterar = input("Deseja alterar ? S/N  : ").upper()
+    if deseja_alterar == "S":
+        print("Voce decidiu alterar")
+    else:
+        print("Você não quer alterar")
+        
 def tela_contas_alterar():
     print(">  Alterar uma conta.")
     print("---------------------")
 
     print("Qual conta deseja alterar ?")
-    base = input("Conta base :  ")
-    complementar = input("Conta complementar :  ")
+    base = input("Conta base :  ").upper()
+    complementar = input("Conta complementar :  ").upper()
     dados_consultar = [base, complementar]
-
+    return(dados_consultar)
+def consultar_conta(dados_consultar):
+    base = dados_consultar[0]
+    complementar = dados_consultar[1]
     conexao = s.connect(bancodados_nome)
     cursor = conexao.cursor()
     cursor.execute("SELECT * FROM contas WHERE conta_base = ? and conta_complementar = ?", (base, complementar))
     resultados = cursor.fetchall()
-    if resultados:
-        for resultado in resultados:
-            print(f"id: {resultado[0]}")
-            print(f"Código base: {resultado[1]}")
-            print(f"Código complementar {resultado[2]}")
-            print(" ")
-    else:
-        print("Não tem resultados")
     cursor.close()
     conexao.close()
+    if resultados:
+        conta_id = resultados[0][0]
+        return (conta_id)
+    else:
+        print("Não tem resultados")
+        return 0
