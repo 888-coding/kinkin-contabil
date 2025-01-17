64d353652bf6993c384da140ad3c0f63231d859a
888-coding <pachelee@gmail.com>
2025-01-17 00:32:44 -0300

    Adicionado o item para Alterar.

--- contas_alterar.py
+++ contas_alterar.py
@@ -1,16 +1,27 @@
 #contas_alterar.py
 import sqlite3 as s
+import colorama
+from colorama import Fore
 from conexao import bancodados_nome
 def alterar():
     dados_consultar = tela_contas_alterar()
     conta_id = consultar_conta(dados_consultar)
-    if conta_id == "0":
+    if conta_id == 0:
         print("Não encontramos a conta.")
     else:
         print("Encontramos a conta !  ")
+        print(conta_id)
         deseja_alterar = input("Deseja alterar ? S/N  : ").upper()
         if deseja_alterar == "S":
-            print("Voce decidiu alterar")
+            print(f"Voce decidiu alterar o conta id : {conta_id}")
+            conexao = s.connect(bancodados_nome)
+            cursor = conexao.cursor()
+            cursor.execute("SELECT * FROM contas WHERE conta_id = ?", (conta_id,))
+            resultados = cursor.fetchall()
+            print(f"Resultado : {resultados[0][1]}.{resultados[0][2]}: {resultados[0][3]}")
+            input_descricao = input("Descrição : ").upper()
+            cursor.close()
+            conexao.close()
         else:
             print("Você não quer alterar")
 def tela_contas_alterar():
@@ -21,7 +32,7 @@ def tela_contas_alterar():
     base = input("Conta base :  ").upper()
     complementar = input("Conta complementar :  ").upper()
     dados_consultar = [base, complementar]
-    return(dados_consultar)
+    return dados_consultar
 def consultar_conta(dados_consultar):
     base = dados_consultar[0]
     complementar = dados_consultar[1]
@@ -33,7 +44,9 @@ def consultar_conta(dados_consultar):
     conexao.close()
     if resultados:
         conta_id = resultados[0][0]
-        return (conta_id)
+        return conta_id
     else:
-        print("Não tem resultados")
+        colorama.init()
+        print(Fore.RED + "Não tem resultados" + Fore.RESET)
+        colorama.deinit()
         return 0
