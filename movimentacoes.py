6b167816034073f58ce091c02fb53bb6d9ea952e
888-coding <pachelee@gmail.com>
2025-01-26 08:34:03 -0300

    Montando a menu de lançamento.

--- movimentacoes.py
+++ movimentacoes.py
@@ -6,3 +6,30 @@ def acessarMovimentacoes():
     colorama.init()
     print(Fore.GREEN + ">> MOVIMENTAÇÕES" + Fore.RESET)
     colorama.deinit()
+    print("1 - Cadastrar movimentação")
+    print("2 - Consultar Movimentação")
+    print("3 - Excluir   movimentação")
+
+    while True:
+        escolha = input("Escolha a opção : ")
+        if escolha.isdigit():
+            escolha = int(escolha)
+            if (escolha == 1) or (escolha == 2) or (escolha == 3) :
+                break
+            else:
+                print("Erro. Escolha novamente.")
+        else:
+            print("Erro. Escolha novamente.")
+    if escolha == 1:
+        cadastrarMovimentacao()
+    elif escolha == 2:
+        consultarMovimentacao()
+    else:
+        excluirMovimentacao()
+
+def cadastrarMovimentacao():
+    print("Nada1")
+def consultarMovimentacao():
+    print("Nada2")
+def excluirMovimentacao():
+    print("Nada3")
