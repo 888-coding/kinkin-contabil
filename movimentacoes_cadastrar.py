7373e770ea6a77b5789e6279cd8edf65b7d7a285
888-coding <pachelee@gmail.com>
2025-01-29 00:22:21 -0300

    Montando a logica de programar cadastro de movimentacao

--- movimentacoes_cadastrar.py
+++ movimentacoes_cadastrar.py
@@ -1,1 +1,12 @@
 # movimentacoes_cadastrar.py
+import colorama
+from colorama import Fore
+
+def cadastrarMovimentacoes():
+    colorama.init()
+    print(Fore.GREEN + "Cadastro de movimentação" + Fore.RESET)
+    colorama.deinit()
+    # 01 : Data de movimentacao
+    # 02 :
+    #   02.1 : movimentacoes , passo por passo.
+    
