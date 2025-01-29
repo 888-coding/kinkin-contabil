70c7741197b2ebe07a12126f6bdc57984cd307a3
888-coding <pachelee@gmail.com>
2025-01-29 10:04:48 -0300

    Data da movimentacao, iniciando o controle da data

--- movimentacoes_cadastrar.py
+++ movimentacoes_cadastrar.py
@@ -2,11 +2,14 @@
 import colorama
 from colorama import Fore
 
-def cadastrarMovimentacoes():
+def TelaCadastrarMovimentacoes():
     colorama.init()
     print(Fore.GREEN + "Cadastro de movimentação" + Fore.RESET)
     colorama.deinit()
     # 01 : Data de movimentacao
     # 02 :
     #   02.1 : movimentacoes , passo por passo.
-    
+    print("*Para cadastro de movimentacao precisa ter contas cadastradas")
+    dataMovimentacao = input("Informe a data de cadastro no formato DD/MM/AAAA : ")
+    # Para data de movimentacao precisa eliminar possíveis erros. 
+TelaCadastrarMovimentacoes()
