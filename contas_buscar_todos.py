34e84cb0acbba82cae0ede49f8869e3d12f3dd71
888-coding <pachelee@gmail.com>
2025-06-15 10:24:34 -0300

    寫成中文comment

--- contas_buscar_todos.py
+++ contas_buscar_todos.py
@@ -14,20 +14,22 @@ def mostratodos():
     colorama.init()
     print(Fore.GREEN + "Lista de todas as contas:" + Fore.RESET)
     colorama.deinit()
-    print("CODIGO BASE      CODIGO COMPLEMENTAR  DESCRICAO           ")
+    print("CODIGO BASE         CODIGO COMPLEMENTAR DESCRICAO           ")
+    # 列印資料庫的內容
     for resultado in resultados:
         coluna = 1
         coluna_maxima = 20
-        i = 1
-        while i < 4:
-            for caracter in resultado[i]:
-                print(f"{caracter}", end="")
+        # NumeroCampoTabela 是指資料庫裡的列表中的資料, 譬如: 1 是 主號碼
+        NumeroCampoTabela = 1
+        while NumeroCampoTabela <= 3:
+            for cada_caracter in resultado[NumeroCampoTabela]:
+                print(f"{cada_caracter}", end="")
                 coluna = coluna + 1
-            caracter = ""
+            cada_caracter = ""
+            # 如果每個coluna 沒有到最高數, 加空白鍵
             while coluna != coluna_maxima:
                 print(" ", end="")
                 coluna = coluna + 1
             coluna = 1
-            i = i + 1
-        print("")
-
+            NumeroCampoTabela = NumeroCampoTabela + 1
+        print("end")
